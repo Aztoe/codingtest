53e544556a18af463c85afa755e8c5eec31f7d47
Aztoe <zltkddn@gmail.com>
2023-09-05 16:02:09 +0900

    [unrated] Title: 순서 바꾸기, Time: 1.92 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,13 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] result = new int[num_list.length];
-        
-        // n 번째 이후의 원소들을 결과 배열의 앞부분에 복사
-        System.arraycopy(num_list, n, result, 0, num_list.length - n);
-        
-        // n 번째까지의 원소들을 결과 배열의 뒷부분에 복사
-        System.arraycopy(num_list, 0, result, num_list.length - n, n);
-        
-        return result;
+            List<Integer> al = new ArrayList<>();
+        int index = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            if (i < n) {
+                al.add(num_list[i]);
+            } else {
+                al.add(index++, num_list[i]);
+            }
+        }
+
+        return al.stream().mapToInt(i -> i).toArray();
     }
 }
