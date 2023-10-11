01f9d43629c7becb70ce0124a19eca5a0f0518c6
Aztoe <zltkddn@gmail.com>
2023-10-11 18:27:36 +0900

    [level 2] Title: 숫자의 표현, Time: 0.41 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924#) 
 
 ### 성능 요약
 
-메모리: 52 MB, 시간: 1.36 ms
+메모리: 51.9 MB, 시간: 0.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 18:19:31
+2023년 10월 3일 18:27:35
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,17 +1,19 @@
 class Solution {
     public int solution(int n) {
         int count = 0;
+        int sum = 0;
+        int start = 1;
 
-        for (int start = 1; start <= n; start++) {
-            int sum = 0;
-            for (int i = start; i <= n; i++) {
-                sum += i;
-                if (sum > n) {
-                    break;
-                } else if (sum == n) {
-                    count++;
-                    break;
-                }
+        for (int end = 1; end <= n; end++) {
+            sum += end;
+
+            while (sum > n) {
+                sum -= start;
+                start++;
+            }
+
+            if (sum == n) {
+                count++;
             }
         }
 
