c6574054cf036d0aa5a96fef31dc5bdcf9c3d33a
Aztoe <zltkddn@gmail.com>
2023-10-20 16:27:23 +0900

    [level 2] Title: 타겟 넘버, Time: 0.38 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,16 @@
 class Solution {
 public int solution(int[] numbers, int target) {
-        return calculateWays(numbers, 0, 0, target);
+        return dfs(numbers, 0, 0, target);
     }
 
-    private int calculateWays(int[] numbers, int index, int sum, int target) {
+    private int dfs(int[] numbers, int index, int sum, int target) {
         if (index == numbers.length) {
             return (sum == target) ? 1 : 0;
         }
 
         int result = 0;
-        result += calculateWays(numbers, index + 1, sum + numbers[index], target);
-        result += calculateWays(numbers, index + 1, sum - numbers[index], target);
+        result += dfs(numbers, index + 1, sum + numbers[index], target);
+        result += dfs(numbers, index + 1, sum - numbers[index], target);
 
         return result;
     }
