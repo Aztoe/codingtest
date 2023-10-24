561fbb9eab9233a8a79b37793c94554a96b3a505
Aztoe <zltkddn@gmail.com>
2023-10-24 17:55:46 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.17 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,63 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        Solution t = new Solution();
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        System.out.println(t.solution(N, K));
+
+    }
+
+    public long solution(long N, long K) {
+
+
+        if (N == 0) {
+            return 0;
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (N > 0) {
+            long num = N % K;
+            sb.insert(0, num); //0 인덱스에 num 삽입
+            N /= K;
+        }
+        int primeCount = countPrimes(sb.toString());
+        return primeCount;
+
+    }
+    public int countPrimes(String s) {
+        int count = 0;
+        String[] numbers = s.split("0");
+        for (String numStr : numbers) {
+            if(!numStr.isEmpty()) {
+                long num = Long.parseLong(numStr);
+                if (isPrime(num)) {
+                    count++;
+            }
+
+            }
+        }
+        return count;
+
+    }
+
+
+    public boolean isPrime(long n) {
+        if(n <= 1) return false;
+        else if(n == 2) return true;
+        for(int i = 2; i <= Math.sqrt(n); i++)
+            if(n % i == 0)
+                return false;
+        return true;
+    }
+
+    }
+
+
