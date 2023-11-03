84b8413e91db0ed7cb6f29b446654e1644dc574e
Aztoe <zltkddn@gmail.com>
2023-11-03 14:57:40 +0900

    [Silver II] Title: 랜선 자르기, Time: 328 ms, Memory: 20000 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+        static int n,m;
+        static  int[] arr;
+
+        public static long solution(int n,int m,int[] arr) throws IOException {
+
+            long start = 1;
+            long end = Arrays.stream(arr).max().getAsInt();
+            long answer =0;
+
+            while (start <= end) {
+                int length = 0;
+
+                long mid = (start + end) / 2;
+                for (int i = 0; i < n; i++) {
+                    length += arr[i] / mid;
+
+                }
+
+                if (length < m) {
+                    end = mid - 1;
+                } else {
+                    start= mid+1;
+                    answer = mid;
+                }
+
+            }
+            return answer;
+        }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(solution(n,m,arr));
+    }
+}
