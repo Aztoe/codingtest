b29b2d3f2cd7601ecf27e8605d7508a63d4c4760
Aztoe <zltkddn@gmail.com>
2023-11-12 12:49:52 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,38 +1,23 @@
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
 
-public class Solution {
+class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-        Queue<String> q1 = new LinkedList<>();
-        Queue<String> q2 = new LinkedList<>();
-        Queue<String> q3 = new LinkedList<>();
-        for (int i = 0; i < cards1.length; i++) {
-            q1.add(cards1[i]);
+        int cardIdx1 = 0;
+        int cardIdx2 = 0;
 
+        for(int i=0; i<goal.length; i++){
+            String target = goal[i];
+
+            if(cardIdx1 < cards1.length && target.equals(cards1[cardIdx1]))
+                cardIdx1 ++;
+            else if (cardIdx2 < cards2.length && target.equals(cards2[cardIdx2]))
+                cardIdx2++;
+            else
+                return "No";
         }
 
-        for (int i = 0; i < cards2.length; i++) {
-            q2.add(cards2[i]);
-        }
-        for (int i = 0; i < goal.length ; i++) {
-            q3.add(goal[i]);
-        }
-       
 
-
-          while ((!q3.isEmpty())) {
-              String str =q3.poll();
-
-              if(str.equals(q1.peek())) q1.poll();
-              else if (str.equals(q2.peek())) { q2.poll();
-                  
-              }else return "No";
-          }
-        
-          return "Yes";
+        return "Yes";
     }
-
-
 }
